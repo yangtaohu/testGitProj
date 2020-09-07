5560f39bfaeaa5ce39aed9b20a26924be0240936
yangpei <yangpei@yy.com>
2020-09-07 18:10:16 +0800

    [新增文件]

---
+++ testGitProj/testView.h
@@ -0,0 +1,17 @@
+//
+//  testView.h
+//  testGitProj
+//
+//  Created by 杨佩 on 2020/9/7.
+//  Copyright © 2020 易家杨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface testView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testGitProj/testView.m
@@ -0,0 +1,21 @@
+//
+//  testView.m
+//  testGitProj
+//
+//  Created by 杨佩 on 2020/9/7.
+//  Copyright © 2020 易家杨. All rights reserved.
+//
+
+#import "testView.h"
+
+@implementation testView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
