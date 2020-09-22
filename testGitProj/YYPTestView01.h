8d13894d1838be70482dfd56e8891695b309adaf
yangpei <yangpei@yy.com>
2020-09-22 10:16:40 +0800

    [新增提交测试文件]

---
+++ testGitProj/YYPTestView01.h
@@ -0,0 +1,17 @@
+//
+//  YYPTestView01.h
+//  testGitProj
+//
+//  Created by 易家杨 on 2020/9/22.
+//  Copyright © 2020 易家杨. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YYPTestView01 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testGitProj/YYPTestView01.m
@@ -0,0 +1,17 @@
+//
+//  YYPTestView01.m
+//  testGitProj
+//
+//  Created by 易家杨 on 2020/9/22.
+//  Copyright © 2020 易家杨. All rights reserved.
+//
+
+#import "YYPTestView01.h"
+
+@implementation YYPTestView01
++(void)load
+{
+    NSLog(@"%s",__func__);
+    
+}
+@end
